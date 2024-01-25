8238d90ed203f48c99f01717f64e1383821dcc55
siljamat <siljama@hotmail.fi>
2024-01-25 12:52:01 +0200

    first commit

---
+++ src/api/controllers/userController.ts
@@ -0,0 +1,215 @@
+import {
+  addUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from '../models/userModel';
+import {Request, Response, NextFunction} from 'express';
+import CustomError from '../../classes/CustomError';
+import bcrypt from 'bcryptjs';
+import {User} from '../../types/DBTypes';
+import {MessageResponse} from '../../types/MessageTypes';
+import {validationResult} from 'express-validator';
+import { error } from 'console';
+const salt = bcrypt.genSaltSync(12);
+
+const userListGet = async (
+  _req: Request,
+  res: Response<User[]>,
+  next: NextFunction
+) => {
+  try {
+    const users = await getAllUsers();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const userGet = async (
+  req: Request<{id: string}, {}, {}>,
+  res: Response<User>,
+  next: NextFunction
+) => {
+  try {
+    const id = Number(req.params.id);
+    const user = await getUser(id);
+    res.json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// TODO: create userPost function to add new user
+// userPost should use addUser function from userModel
+// userPost should use validationResult to validate req.body
+// - user_name should be at least 3 characters long
+// - email should be a valid email
+// - password should be at least 5 characters long
+// userPost should use bcrypt to hash password
+const userPost = async (
+  req: Request<{}, {}, Omit<User, 'user_id'>>,
+  res: Response<MessageResponse>,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('cat_post validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+
+  try {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, salt);
+    const result = await addUser(user);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const userPut = async (
+  req: Request<{id: number}, {}, User>,
+  res: Response<MessageResponse>,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('user_put validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+
+  try {
+    if (req.user && (req.user as User).role !== 'admin') {
+      throw new CustomError('Admin only', 403);
+    }
+
+    const user = req.body;
+
+    const result = await updateUser(user, req.params.id);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// TODO: create userPutCurrent function to update current user
+// userPutCurrent should use updateUser function from userModel
+// userPutCurrent should use validationResult to validate req.body
+const userPutCurrent = async (
+  req: Request<User>,
+  res: Response<MessageResponse>,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('user_post validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+
+  try {
+    const id = (req.user as User).user_id;
+    const result = await updateUser(req.body, id);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+// TODO: create userDelete function for admin to delete user by id
+// userDelete should use deleteUser function from userModel
+// userDelete should use validationResult to validate req.params.id
+// userDelete should use req.user to get role
+const userDelete = async (
+  req: Request<{id: string}>,
+  res: Response<MessageResponse>,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('cat_post validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+
+  try {
+    if (req.user && (req.user as User).role !== 'admin') {
+      throw new CustomError('Admin only', 403);
+    }
+
+    const id = Number(req.params.id);
+    const result = await deleteUser(id);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+}
+
+const userDeleteCurrent = async (
+  req: Request,
+  res: Response<MessageResponse>,
+  next: NextFunction
+) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const messages: string = errors
+      .array()
+      .map((error) => `${error.msg}: ${error.param}`)
+      .join(', ');
+    console.log('cat_post validation', messages);
+    next(new CustomError(messages, 400));
+    return;
+  }
+
+  try {
+    const id = (req.user as User).user_id;
+    const result = await deleteUser(id);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const checkToken = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.user) {
+    next(new CustomError('token not valid', 403));
+  } else {
+    res.json(req.user);
+  }
+};
+
+export {
+  userListGet,
+  userGet,
+  userPost,
+  userPut,
+  userPutCurrent,
+  userDelete,
+  userDeleteCurrent,
+  checkToken,
+};
---
+++ src/api/models/catModel.ts
@@ -0,0 +1,107 @@
+import {promisePool} from '../../database/db';
+import CustomError from '../../classes/CustomError';
+import {ResultSetHeader, RowDataPacket} from 'mysql2';
+import {Cat} from '../../types/DBTypes';
+import {MessageResponse, UploadResponse} from '../../types/MessageTypes';
+
+const getAllCats = async (): Promise<Cat[]> => {
+  const [rows] = await promisePool.execute<RowDataPacket[] & Cat[]>(
+    `
+    SELECT cat_id, cat_name, weight, filename, birthdate, ST_X(coords) as lat, ST_Y(coords) as lng,
+    JSON_OBJECT('user_id', sssf_user.user_id, 'user_name', sssf_user.user_name) AS owner 
+	  FROM sssf_cat 
+	  JOIN sssf_user 
+    ON sssf_cat.owner = sssf_user.user_id
+    `
+  );
+  if (rows.length === 0) {
+    throw new CustomError('No cats found', 404);
+  }
+  const cats = (rows as Cat[]).map((row) => ({
+    ...row,
+    owner: JSON.parse(row.owner?.toString() || '{}'),
+  }));
+
+  return cats;
+};
+
+// TODO: create getCat function to get single cat
+const getCat = async (catId: number): Promise<Cat> => {
+  const [rows] = await promisePool.execute<RowDataPacket[] & Cat[]>(
+    `
+    SELECT * FROM sssf_cat WHERE cat_id = ?;
+    `,
+    [catId]
+  );
+  if (rows.length === 0) {
+    throw new CustomError('No cats found', 404);
+  };
+  return rows[0];
+};
+
+
+// TODO: use Utility type to modify Cat type for 'data'.
+// Note that owner is not User in this case. It's just a number (user_id)
+const addCat = async (data: Cat): Promise<UploadResponse> => {
+  const [headers] = await promisePool.execute<ResultSetHeader>(
+    `
+    INSERT INTO sssf_cat (cat_name, weight, owner, filename, birthdate, coords) 
+    VALUES (?, ?, ?, ?, ?, POINT(?, ?))
+    `,
+    [
+      data.cat_name,
+      data.weight,
+      data.owner,
+      data.filename,
+      data.birthdate,
+      data.lat,
+      data.lng,
+    ]
+  );
+  if (headers.affectedRows === 0) {
+    throw new CustomError('No cats added', 400);
+  }
+  return {message: 'Cat added', id: headers.insertId};
+};
+
+// TODO: create updateCat function to update single cat
+// if role is admin, update any cat
+// if role is user, update only cats owned by user
+// You can use updateUser function from userModel as a reference for SQL
+const updateCat = async ( 
+  data: Partial<Cat>,
+  catId: number,
+  userId: number,
+  role: string
+): Promise<MessageResponse> => {
+  const sql = promisePool.format(
+    `
+    UPDATE sssf_cat 
+    SET ? 
+    WHERE cat_id = ? 
+    AND (owner = ? OR ? = 'admin');
+    `,
+    [data, catId, userId, role]
+  );
+  const [headers] = await promisePool.execute<ResultSetHeader>(sql);
+  if (headers.affectedRows === 0) {
+    throw new CustomError('No cats updated', 400);
+  }
+  return {message: 'Cat updated'};
+}
+
+const deleteCat = async (catId: number): Promise<MessageResponse> => {
+  const [headers] = await promisePool.execute<ResultSetHeader>(
+    `
+    DELETE FROM sssf_cat 
+    WHERE cat_id = ?;
+    `,
+    [catId]
+  );
+  if (headers.affectedRows === 0) {
+    throw new CustomError('No cats deleted', 400);
+  }
+  return {message: 'Cat deleted'};
+};
+
+export {getAllCats, addCat, deleteCat, updateCat, getCat};
